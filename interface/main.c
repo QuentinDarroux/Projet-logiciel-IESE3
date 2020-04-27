8bb7149a7fa0e8f6b9eb1e3d27bac885d81cab52
escallim <mathisescallier@gmail.com>
2020-04-27 08:34:54 +0200

    mise a jour interface : son, anim supp

--- interface/main.c
+++ interface/main.c
@@ -1,29 +1,64 @@
 //MAC OS, Linux : gcc main.c -o prog $(sdl2-config --cflags --libs) -> ./prog
 
-//#include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
+#include <unistd.h> //temporisation
+#include <stdbool.h>
+//audio
+#include "audio/audio.c"
+#include "audio/audio.h"
 
 //#include <SDL_image.h>
-#include <stdbool.h>
+//#include <SDL_ttf.h>
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define FPS_LIMIT 16 		//60 fps
 
+//++Variables globales++//
+
+/*ACCEUIL*/
+int CLIGNOTEMENT=0;
 /*MORPION*/
-//Variables globales
 int CROIX1=0;
-int CROIX2=0;
-int CROIX3=0;
-int CROIX4=0;
-int CROIX5=0;
-int CROIX6=0;
-int CROIX7=0;
-int CROIX8=0;
-int CROIX9=0;
+	int CROIX2=0;
+	int CROIX3=0;
+	int CROIX4=0;
+	int CROIX5=0;
+	int CROIX6=0;
+	int CROIX7=0;
+	int CROIX8=0;
+	int CROIX9=0;
+/*PENDU*/
+int A=0;
+	int B=0;
+	int C=0;
+	int D=0;
+	int E=0;
+	int F=0;
+	int G=0;
+	int H=0;
+	int I=0;
+	int J=0;
+	int K=0;
+	int L=0;
+	int M=0;
+	int N=0;
+	int O=0;
+	int P=0;
+	int Q=0;
+	int R=0;
+	int S=0;
+	int T=0;
+	int U=0;
+	int V=0;
+	int W=0;
+	int X=0;
+	int Y=0;
+	int Z=0;
+	int _=0;
 
 //Fonction erreur
 void SDL_ExitError(const char *message);
@@ -41,12 +76,20 @@ int position(int x_min, int x_max, int y_min, int y_max, SDL_Event event);
 
 //Animation Bouton accueil
 int anim_bouton(int x_min, int x_max, int y_min, int y_max, SDL_Event event, int affiche_clear, SDL_Texture *texture);
+//Animation Select a game (prototype obligatoire pour timer)
+Uint32 clignotement_select_timer(Uint32 intervalle, void *parametre);
+
+//audio-musique
+void audio_musique(int set, int play, const char* file, SDL_Renderer *renderer, SDL_Window *window);
 
 /*PROTOTYPES MORPION*/
 //Selection morpion
 void affiche_point_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event *event1);
 //Affichage croix
-void croix_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event *event1);
+void croix_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event *event1, int restart);
+
+/*PROTOTYPES PENDU*/
+void affiche_texte(char* chaine, SDL_Renderer *renderer, SDL_Window *window, int clear);
 
 //limitation fps
 void limit_fps(unsigned int limit);
@@ -71,7 +114,7 @@ int main(int argc, char** argv){
 	 frame_limit = SDL_GetTicks() + FPS_LIMIT;
 
 	//initialistion de la SDL
-	if(SDL_Init(SDL_INIT_VIDEO) != 0){
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		 SDL_ExitError("Initialisation echouee"); //affiche l'erreur
 	}
 
@@ -86,14 +129,24 @@ int main(int argc, char** argv){
 	if(renderer == NULL)
 		SDL_ExitError("Rendu fenetre echouee");
 
+	/*clignotement select a game*/
+	SDL_TimerID timer;
+	timer = SDL_AddTimer(500, clignotement_select_timer, NULL);//parametre : (nb de secondes entre chaque appel, fonction appelé, parametre pour la fonction)
+    SDL_Surface *image_cli = NULL;
+	SDL_Texture *texture_cli = NULL;
+	SDL_Rect pos_cli; pos_cli.x = 490; pos_cli.y = 530; pos_cli.h = 40;	pos_cli.w = 400;
+
+
+	/*initialisation audio*/
+	initAudio();
+	int musique_accueil = 1;
+	audio_musique(1, musique_accueil, "music/jul-cross.wav", renderer, window);	//initialement on lance la musique d'accueil
+
     //** EVENEMENTS **//
 
     SDL_bool programmed_launched = SDL_TRUE; //boolean SDL
 
     while(programmed_launched){
-
-    	SDL_Event event;					//variable evenement
-    	while(SDL_PollEvent(&event)){		//capture des evenemets 
 
     		//affichage page acceuil
     		SDL_Surface *image = NULL;
@@ -104,6 +157,13 @@ int main(int argc, char** argv){
 			pos_image.h = WINDOW_HEIGHT;
 			pos_image.w = WINDOW_WIDTH;
     		affiche_image(image, texture, pos_image, "image/page_accueil.bmp", renderer, window);
+
+    		//clignotement select
+			if(CLIGNOTEMENT)
+    			affiche_image(image_cli, texture_cli, pos_cli, "image/cache_select.bmp", renderer, window);
+    		else
+    			clear_image(NULL, NULL, texture_cli);
+    		//SDL_RemoveTimer(timer); /* Arrêt du timer */
     		
     		//affichage case Jeu 1
     		SDL_Surface *image_choix1 = NULL;
@@ -113,7 +173,7 @@ int main(int argc, char** argv){
 			pos_choix1.w = 300;
 			pos_choix1.x = (WINDOW_WIDTH - pos_choix1.w - 925) / 2;
 			pos_choix1.y = (WINDOW_HEIGHT - pos_choix1.h + 60) / 2;
-
+			//animation bouton
 			int anim_selec_morpion;
 			if(anim_selec_morpion == 1)
 				affiche_image(image_choix1, texture_choix1, pos_choix1, "image/choix_morpion.bmp", renderer, window);
@@ -126,7 +186,7 @@ int main(int argc, char** argv){
 			pos_choix2.w = 300;
 			pos_choix2.x = (WINDOW_WIDTH - pos_choix2.w - 95) / 2;
 			pos_choix2.y = (WINDOW_HEIGHT - pos_choix2.h + 60) / 2;
-
+			//animation bouton
 			int anim_selec_pendu;
 			if(anim_selec_pendu == 1)
 				affiche_image(image_choix2, texture_choix2, pos_choix2, "image/choix_pendu.bmp", renderer, window);
@@ -139,13 +199,21 @@ int main(int argc, char** argv){
 			pos_choix3.w = 300;
 			pos_choix3.x = (WINDOW_WIDTH - pos_choix3.w + 730) / 2;
 			pos_choix3.y = (WINDOW_HEIGHT - pos_choix3.h + 60) / 2;
-			
+			//animation bouton
 			int anim_selec_cheval;
 			if(anim_selec_cheval == 1)
 				affiche_image(image_choix3, texture_choix3, pos_choix3, "image/choix_cheval.bmp", renderer, window);
 
+			//etat de la musique
+			audio_musique(0, musique_accueil, "music/jul-cross.wav", renderer, window);	//on lit la musique d'accueil
+		    
 		    //affichage rendu page accueil
 		    SDL_RenderPresent(renderer); 
+
+
+
+		    SDL_Event event;					//variable evenement
+    		while(SDL_PollEvent(&event)){		//capture des evenemets 
 
     		switch(event.type){
 
@@ -184,6 +252,14 @@ int main(int argc, char** argv){
     				if(event.button.clicks >= 2)	
     					printf("Double Clic ! \n"); 								//double clic
 
+    				//* musique *//
+    				if(clic_gauche(25,115,626,715,event)){
+    					musique_accueil ^= 1;
+    					printf("musique : %d", musique_accueil);
+    					audio_musique(0, musique_accueil, "music/jul-cross.wav", renderer, window);
+    				}
+
+
     				//**-- MORPION --**//
     				
 
@@ -199,31 +275,26 @@ int main(int argc, char** argv){
 						affiche_image(image_fond_morpion, texture_fond_morpion, pos_image, "image/morpion.bmp", renderer, window);
 						SDL_RenderPresent(renderer);
 
-
-
 		    				SDL_Event event1;	
-		    				event1.button.button = 0;
-		    				event1.button.button = 0;
-		    				event1.motion.x = 0;
-		    				event1.motion.y = 0;
 
-		    				printf("BOUTON BUUUG : %d \n\n", event1.button.button);
+		    				int first_loop = 1;	//identification de la premiere boucle (BUG croix)
 
 		    					while(morpion_running == 1){					//boucle de jeu morpion
-
+		    						
 		    						while(SDL_PollEvent(&event1)){
 				    						
 		    							SDL_RenderPresent(renderer);
 		    							affiche_image(image_fond_morpion, texture_fond_morpion, pos_image, "image/morpion.bmp", renderer, window);
-		    							croix_morpion(renderer, window, &event1);
+		    							if(first_loop == 0)
+		    							croix_morpion(renderer, window, &event1, 0);
 
 				    						switch(event1.type){
 
 
 
 				    							case SDL_MOUSEBUTTONDOWN:						//clic de la souris
-
-													croix_morpion(renderer, window, &event1);
+				    								
+				    								croix_morpion(renderer, window, &event1, 0);
 
 				    								if(clic_gauche(1167, 1257, 8, 74, event1)){	//quitte le jeu (croix)
 				    									morpion_running = 0;
@@ -255,8 +326,10 @@ int main(int argc, char** argv){
 
 
 		    								}
-
+		    								
+		    								first_loop = 0;
 		    						}
+
 		    					}
 		    			clear_image(NULL, NULL, texture_fond_morpion);
     				}
@@ -272,26 +345,43 @@ int main(int argc, char** argv){
     					//affichage image de fond du jeu
     					SDL_Surface *image_fond_pendu = NULL;
 						SDL_Texture *texture_fond_pendu = NULL;
-						affiche_image(image_fond_pendu, texture_fond_pendu, pos_image, "image/pendu.bmp", renderer, window);
-						SDL_RenderPresent(renderer);
+						//affiche_image(image_fond_pendu, texture_fond_pendu, pos_image, "image/pendu.bmp", renderer, window);
+						//SDL_RenderPresent(renderer);
 
+						//affichage de texte
+						char texte[30] = "BINKSTOB_NKS";
 
 		    				SDL_Event event2;									
 		    					while(pendu_running == 1){					//boucle de jeu morpion
+
+									SDL_RenderPresent(renderer);
+									affiche_image(image_fond_pendu, texture_fond_pendu, pos_image, "image/pendu.bmp", renderer, window);
+									affiche_texte(texte, renderer, window, 0);
+									//texte[0] = '\0';
 
 		    						while(SDL_PollEvent(&event2)){
 				    						
 				    						switch(event2.type){
 
 
-
 				    							case SDL_MOUSEBUTTONDOWN:
+													
+				    								if(clic_gauche(62, 242,645,681, event2)){		//restart
+				    									texte[0] = '\0';
+				    									affiche_texte(texte, renderer, window, 1);
+				    								}
 
-		
 				    								if(clic_gauche(1167, 1257, 8, 74, event2)){	//quitte le jeu (croix)
 				    									pendu_running = 0;
+				    									affiche_texte(texte, renderer, window, 1);
 				    									printf("FIN PENDU \n");
 				    								}
+
+				    								break;
+
+				    							case SDL_MOUSEMOTION:							//position de la souris
+					    							
+					    							printf("position souris : %d : %d \n", event2.motion.x, event2.motion.y);
 
 				    								break;
 
@@ -299,6 +389,7 @@ int main(int argc, char** argv){
 				    								programmed_launched = SDL_FALSE; 
 				    								pendu_running = 0;
 				    								printf("FIN PENDU \n");
+				    								affiche_texte(texte, renderer, window, 1);
 				    								clear_image(NULL, NULL, texture_choix1);	//liberation texture case1
     												clear_image(NULL, NULL, texture_choix2);	//liberation texture case2
 									    			clear_image(NULL, NULL, texture_choix3);	//liberation texture case3
@@ -400,14 +491,14 @@ int main(int argc, char** argv){
 
     			case SDL_KEYDOWN: 								//appuie touche
 					switch(event.key.keysym.sym){
-						case SDLK_b: printf("Appuie B\n"); continue;	//appuie touche B
-						default: continue;
+						case SDLK_b: printf("Appuie B\n"); break;	//appuie touche B
+						default: break;
 					}
 
 				case SDL_KEYUP: 								//relachement touche
 					switch(event.key.keysym.sym){
-						case SDLK_b: printf("Relache B\n"); continue;	//relache touche B
-						default: continue;
+						case SDLK_b: printf("Relache B\n"); break;	//relache touche B
+						default: break;
 					}
 
     			case SDL_QUIT: 
@@ -418,7 +509,7 @@ int main(int argc, char** argv){
     			clear_image(NULL, NULL, texture);			//liberation texture fond accueil
     			break;		//quitte le programme (croix)
 
-    			default:  continue;	//affichage par defaut jul
+    			default:  break;	//affichage par defaut jul
 
     		}
     	}	
@@ -439,8 +530,10 @@ int main(int argc, char** argv){
 
     //test FPS
     //frame_limit = SDL_GetTicks() + FPS_LIMIT;
-   
-    //fonction clear
+   		//arret musique
+    	endAudio();
+   		//arret timer
+    	SDL_RemoveTimer(timer);
 		//fermeture du rendu
 		SDL_DestroyRenderer(renderer);
 		//fermeture de la fenêtre
@@ -500,7 +593,7 @@ void clear_image(SDL_Window *w, SDL_Renderer *r, SDL_Texture *t){
 		SDL_DestroyRenderer(r);
 	if(w != NULL)
 		SDL_DestroyWindow(w);
-	printf("clear\n");
+	//printf("clear\n");
 	//IMG_Quit();
 }
 
@@ -513,7 +606,7 @@ int clic_gauche(int x_min, int x_max, int y_min, int y_max, SDL_Event event){
 	    		if(event.button.button == SDL_BUTTON_LEFT)
 	    			return 1;
 	
-    return 0; //default
+    	return 0; //default
 }
 
 //POSITION DANS UN ESPACE (a utiliser dans MOUSEMOTION)
@@ -529,7 +622,7 @@ int position(int x_min, int x_max, int y_min, int y_max, SDL_Event event){
 
 //AFFICHAGE CROIX MORPION
 
-void croix_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event *event1){
+void croix_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event *event1, int restart){
 
 	SDL_Surface *image_croix;
  	SDL_Texture *texture_croix;
@@ -615,6 +708,13 @@ void croix_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event *event1
 		pos_croix.x = 820;
 		pos_croix.y = 530;
 		affiche_image(image_croix, texture_croix, pos_croix, "image/croix_morpion.bmp", renderer, window);
+	}
+
+	//debut du jeu
+	if(restart){
+		CROIX1 = 0; CROIX2 = 0; CROIX3 = 0; CROIX4 = 0; CROIX5 = 0; CROIX6 = 0; CROIX7 = 0; CROIX8 = 0; CROIX9 = 0; 
+		clear_image(NULL, NULL, texture_croix);
+		printf("clear croix\n");
 	}
 
 	//liberation des croix (restart, quitte, morpion ou quitte jeu)
@@ -707,6 +807,7 @@ void affiche_point_morpion(SDL_Renderer *renderer, SDL_Window *window, SDL_Event
 
 }
 
+/*Fonction animation Bouton accueil*/
 int anim_bouton(int x_min, int x_max, int y_min, int y_max, SDL_Event event, int affiche_clear, SDL_Texture *texture){
 
    if(position(x_min, x_max, y_min, y_max, event))
@@ -720,6 +821,128 @@ int anim_bouton(int x_min, int x_max, int y_min, int y_max, SDL_Event event, int
     	}	
 
 }
+
+/* Fonction clignaotement select callback (sera appelée toutes les 1000 ms) */
+Uint32 clignotement_select_timer(Uint32 intervalle, void *parametre)
+{
+	CLIGNOTEMENT ^= 1;
+    return intervalle;
+}
+
+
+/*fonction qui affiche du texte*/
+
+
+
+void affiche_texte(char* chaine, SDL_Renderer *renderer, SDL_Window *window, int clear){
+
+
+
+	int i = 0;
+
+	SDL_Surface *image_lettre;
+ 	SDL_Texture *texture_lettre;
+  	SDL_Rect pos_lettre;
+	pos_lettre.h = 30;
+	pos_lettre.w = 30;
+	pos_lettre.x = 90;
+	pos_lettre.y = 360;
+
+
+	while(chaine[i] != '\0'){
+
+		switch(chaine[i]){
+
+
+			case 'A':  A = i+1; pos_lettre.x = 70 + (A*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/A.bmp", renderer, window); break;
+			case 'B':  B = i+1; pos_lettre.x = 70 + (B*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/B.bmp", renderer, window); break;
+			case 'C':  C = i+1; pos_lettre.x = 70 + (C*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/C.bmp", renderer, window); break;
+			case 'D':  D = i+1; pos_lettre.x = 70 + (D*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/D.bmp", renderer, window); break;
+			case 'E':  E = i+1; pos_lettre.x = 70 + (E*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/E.bmp", renderer, window); break;
+			case 'F':  F = i+1; pos_lettre.x = 70 + (F*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/F.bmp", renderer, window); break;
+			case 'G':  G = i+1; pos_lettre.x = 70 + (G*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/G.bmp", renderer, window); break;
+			case 'H':  H = i+1; pos_lettre.x = 70 + (H*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/H.bmp", renderer, window); break;
+			case 'I':  I = i+1; pos_lettre.x = 70 + (I*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/I.bmp", renderer, window); break;
+			case 'J':  J = i+1; pos_lettre.x = 70 + (J*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/J.bmp", renderer, window); break;
+			case 'K':  K = i+1; pos_lettre.x = 70 + (K*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/K.bmp", renderer, window); break;
+			case 'L':  L = i+1; pos_lettre.x = 70 + (L*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/L.bmp", renderer, window); break;
+			case 'M':  M = i+1; pos_lettre.x = 70 + (M*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/M.bmp", renderer, window); break;
+			case 'N':  N = i+1; pos_lettre.x = 70 + (N*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/N.bmp", renderer, window); break;
+			case 'O':  O = i+1; pos_lettre.x = 70 + (O*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/O.bmp", renderer, window); break;
+			case 'P':  P = i+1; pos_lettre.x = 70 + (P*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/P.bmp", renderer, window); break;
+			case 'Q':  Q = i+1; pos_lettre.x = 70 + (Q*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/Q.bmp", renderer, window); break;
+			case 'R':  R = i+1; pos_lettre.x = 70 + (R*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/R.bmp", renderer, window); break;
+			case 'S':  S = i+1; pos_lettre.x = 70 + (S*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/S.bmp", renderer, window); break;
+			case 'T':  T = i+1; pos_lettre.x = 70 + (T*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/T.bmp", renderer, window); break;
+			case 'U':  U = i+1; pos_lettre.x = 70 + (U*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/U.bmp", renderer, window); break;
+			case 'V':  V = i+1; pos_lettre.x = 70 + (V*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/V.bmp", renderer, window); break;
+			case 'W':  W = i+1; pos_lettre.x = 70 + (W*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/W.bmp", renderer, window); break;
+			case 'X':  X = i+1; pos_lettre.x = 70 + (X*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/X.bmp", renderer, window); break;
+			case 'Y':  Y = i+1; pos_lettre.x = 70 + (Y*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/Y.bmp", renderer, window); break;
+			case 'Z':  Z = i+1; pos_lettre.x = 70 + (Z*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/Z.bmp", renderer, window); break;
+			case '_':  _ = i+1; pos_lettre.x = 70 + (_*55); affiche_image(image_lettre, texture_lettre, pos_lettre, "image/alphabet/_.bmp", renderer, window); break;
+
+			default: break;
+
+		}
+
+	i++;
+
+	}
+
+
+
+	//liberation de la memoire
+	if(clear == 1){
+		A=0;B=0;C=0;D=0;E=0;F=0;G=0;H=0;I=0;J=0;K=0;L=0;M=0;N=0;O=0;P=0;Q=0;R=0;S=0;T=0;U=0;V=0;W=0;X=0;Y=0;Z=0;_=0;
+		printf("clear texte\n");
+		clear_image(NULL, NULL, texture_lettre);
+	}
+
+}
+
+
+
+
+//Musique-Audio
+void audio_musique(int set, int play, const char* file, SDL_Renderer *renderer, SDL_Window *window){
+
+	SDL_Surface *image_son;
+ 	SDL_Texture *texture_son;
+  	SDL_Rect pos_son;
+	pos_son.h = 50;
+	pos_son.w = 50;
+	pos_son.x = 68;
+	pos_son.y = 648;
+
+	if(set)
+		playMusic(file, SDL_MIX_MAXVOLUME);
+
+	if(play){
+		unpauseAudio();
+		affiche_image(image_son, texture_son, pos_son, "image/son-on.bmp", renderer, window);
+	}
+	if(play == 0){
+		pauseAudio();
+		affiche_image(image_son, texture_son, pos_son, "image/son-off.bmp", renderer, window);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
