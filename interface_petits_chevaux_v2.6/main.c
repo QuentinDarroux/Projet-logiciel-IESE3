975b5cb27df447ee606e8c1447635a609dc7c397
alexandre_BOUHELIER <alexandre.bouhelier25@gmail.com>
2020-05-16 00:50:38 +0200

    code un peu plus propre v2.6

--- interface_petits_chevaux_v2.6/main.c
+++ interface_petits_chevaux_v2.6/main.c
@@ -91,14 +91,12 @@ int anim_bouton2(int x_min, int x_max, int y_min, int y_max, SDL_Event event, in
 /*fonctions petits chevaux*/
 
 void	init_partie(int nb_joueurs, JOUEUR *tab_j, int* id_joueur);
-void	afficher_plateau(JOUEUR* j, PILE *pile_p, int compteur_tours, int i, int nb_joueurs, int de);
 void	copie_partie(PARTIE *partie, int id_joueur, int de, JOUEUR *tab_j);
 void	liste_actions_possibles(JOUEUR *tab_j, int num_j, int de);
 int		jet();
 void	sortir(JOUEUR *tab_j, int nb_joueurs, int num_j, int num_p);
 void	tour(JOUEUR *tab_j, int nb_joueurs, int num_j, int num_p, int de);
 void	centre(JOUEUR *tab_j, int nb_joueurs, int num_j, int num_p, int de);
-void	jouer_de(JOUEUR* tab_j, PARTIE partie, PILE *pile_p, int id_joueur, int nb_joueurs, int de, char* rep,int* retour, int* quitter_partie, int* victoire);
 
 /*auvegardes*/
 
@@ -230,6 +228,7 @@ int main(int argc, char** argv){
 	int slct_retour=0;
 	int slct_oui=0;
 	int slct_non=0;
+
 	int menu=0;
 
 	SDL_Surface *image = NULL;
@@ -640,28 +639,7 @@ int main(int argc, char** argv){
 
 			switch(event.type){
 
-
-				//--- FENETRE ---//
-				//case SDL_WINDOWEVENT:
-
-	    			//redimensionnement de la fenêtre + position de la fenetre
-					//event.window.event
-					//event.window.data1	//nouvelle largeur	//nouvelle position x
-					//event.window.data2	//nouvelle hauteur	//nouvelle position y
-
-					//if(event.window.event == SDL_WINDOWEVENT_LEAVE)					//sortie de la fenetre
-					//	printf("La souris est sortie de la fenêtre\n"); 			
-
-	    			//if(event.window.event == SDL_WINDOWEVENT_ENTER)					//entree dans la fenetre
-					//	printf("La souris est entrée dans la fenêtre\n");
-
-					//break;
-
-
-
-	    		//---- SOURIS ----//
-
-	    		case SDL_MOUSEBUTTONUP:						//clic souris
+	    		case SDL_MOUSEBUTTONUP :		//relache clic gauche souris
 
 					switch(menu)
 					{
@@ -1155,18 +1133,7 @@ int main(int argc, char** argv){
 
 					break;
 
-				case SDL_MOUSEBUTTONDOWN:						//clic souris
-					//coordonnée du clic
-					//if(event.button.button == SDL_BUTTON_LEFT)						//clic gauche
-					//	printf("Clic gauche ");
-
-					//if(event.button.button == SDL_BUTTON_RIGHT)	{					//clic droit
-	    			//	printf("Clic droit ");	
-					//}
-					//if(event.button.clicks >= 2)	
-					//	printf("Double Clic "); 								//double clic
-
-					//printf("en %d : %d\n", event.button.x, event.button.y);	//position du clic
+				case SDL_MOUSEBUTTONDOWN :	//appuie clic gauche
 
 					switch(menu)
 					{
@@ -1241,11 +1208,8 @@ int main(int argc, char** argv){
 
 					break;
 
-				case SDL_MOUSEMOTION: 							//position de la souris
-	    			//position x, position y de la souris
-					//printf("position souris : %d : %d \n", event.motion.x, event.motion.y);
-					//position x relative (deplacement), position y relative (deplacement)
-					//printf("position souris relative : %d : %d \n", event.motion.xrel, event.motion.yrel);
+				case SDL_MOUSEMOTION :	//position de la souris
+
 					switch(menu)
 					{
 						case 0 : //menu principale
@@ -1319,7 +1283,7 @@ int main(int argc, char** argv){
 
 					break;
 
-	    		case SDL_QUIT: 
+	    		case SDL_QUIT :
 					programme_launched = SDL_FALSE; 
 					clear_image(NULL, NULL, texture);
 					break;		//quitte le programme (croix)
@@ -1663,43 +1627,6 @@ void	init_partie(int nb_joueurs, JOUEUR *tab_j, int* id_joueur)
 
 
 
-void	afficher_plateau(JOUEUR* tab_j, PILE *pile_p, int compteur_tours, int id_joueur, int nb_joueurs, int de)
-{
-	int i,j;
-
-	printf("\e[1;1H\e[2J");
-	printf("tour : %d  | pile : %d",compteur_tours,pile_p->sommet);
-
-	printf("\n");
-	for(i=0;i<nb_joueurs;i++){
-	printf("pions de %s :\n",tab_j[i].nom);
-		for(j=0;j<NB_PIONS;j++){
-		printf("\t%d : ",j+1);
-			switch(tab_j[i].p[j].etat){
-				case 0:
-					printf("ecurie\n");
-					break;
-				case 1:
-					printf("tour\t\t%d\n",tab_j[i].p[j].tour);
-					break;
-				case 2:
-					printf("centre\t\t%d\n",tab_j[i].p[j].centre);
-					break;
-				default:
-					break;
-			}
-		}
-	}
-	printf("\njoueur :\t\t\t%s\njet :\t\t\t\t%d\n\n",tab_j[id_joueur].nom,de);
-}
-
-
-
-
-
-
-
-
 
 void	copie_partie(PARTIE *partie, int id_joueur, int de, JOUEUR *tab_j)
 {
@@ -1786,13 +1713,12 @@ void	sortir(JOUEUR *tab_j, int nb_joueurs, int num_j, int num_p)
 	/*on verifie si le pion doit manger un pion en sortant*/
 
 	for(i=0;i<nb_joueurs;i++){
-		if(i==num_j) continue;
-		else{
+		if(i!=num_j)
+		{
 			for(j=0;j<NB_PIONS;j++){
 				if((tab_j[i].p[j].tour==tab_j[num_j].depart)&&(tab_j[i].p[j].tour!=0)){
 					tab_j[i].p[j].tour=0;
 					tab_j[i].p[j].etat=0;
-					printf("\nle pion %d de %s s'est fait mange\n",j+1,tab_j[i].nom);
 				}
 			}
 		}
@@ -1838,15 +1764,14 @@ void	tour(JOUEUR *tab_j, int nb_joueurs, int num_j, int num_p, int de)
 	/*verification : le pion peut en manger un autre en se deplacant*/
 
 	for(i=0;i<nb_joueurs;i++){
-		if(i==num_j) continue;
-		else{
+		if(i!=num_j)
+		{
 			for(j=0;j<NB_PIONS;j++){
 				if(tab_j[i].p[j].tour>=case_actuelle) ecart=tab_j[i].p[j].tour-case_actuelle;
 				else ecart=56-case_actuelle+tab_j[i].p[j].tour;
 				if((ecart<=de)&&(tab_j[i].p[j].tour!=0)){
 					tab_j[i].p[j].tour=0;
 					tab_j[i].p[j].etat=0;
-					printf("\nle pion %d de %s s'est fait mange\n",j+1,tab_j[i].nom);
 				}
 			}
 		}
@@ -1877,55 +1802,7 @@ void	centre(JOUEUR *tab_j, int nb_joueurs, int num_j, int num_p, int de)
 
 
 
-void	jouer_de(JOUEUR* tab_j, PARTIE partie, PILE *pile_p, int id_joueur, int nb_joueurs, int de, char* rep, int* retour, int* quitter_partie, int* victoire)
-{
-	int i,j;
-	int test_aucune_action=0;
-	int test_victoire=0;
-	char commande_pion[MAX_REP];
-	commande_pion[0]='p';
-	commande_pion[2]='\0';
 
-	liste_actions_possibles(tab_j,id_joueur,de);
-	/*proposition au joueur des actions possibles*/
-	printf("action(s) possible(s) :\n");
-	for(i=0;i<NB_PIONS;i++){
-		if(tab_j[id_joueur].p[i].d[0]==1)	printf("sortir le pion %d\t\tp%d\n",i+1,i+1);
-		if(tab_j[id_joueur].p[i].d[1]==1)	printf("faire avancer le pion %d\t\tp%d\n",i+1,i+1);
-		if(tab_j[id_joueur].p[i].d[2]==1)	printf("faire avancer le pion %d\t\tp%d\n",i+1,i+1);
-		if(tab_j[id_joueur].p[i].d[3]==1)	test_aucune_action++;
-	}
-	
-	if(test_aucune_action==NB_PIONS) printf("aucune action possible\t\tok\n");
-	if(!pile_est_vide(pile_p))			printf("retour en arriere\t\tr\n");
-	printf("quitter\t\t\t\tq\n\n");	/*le joueur peut quitter la partie a tout moment*/
-	/*le joueur selectionne l'action*/
-	do{
-		printf("action choisit :\t\t");
-		scanf("%s",rep);
-	} while(!((!strcmp(rep,"p1")&&(tab_j[id_joueur].p[0].d[0]==1||tab_j[id_joueur].p[0].d[1]==1||tab_j[id_joueur].p[0].d[2]==1))||(!strcmp(rep,"p2")&&(tab_j[id_joueur].p[1].d[0]==1||tab_j[id_joueur].p[1].d[1]==1||tab_j[id_joueur].p[1].d[2]==1))||(!strcmp(rep,"ok")&&(tab_j[id_joueur].p[0].d[3]==1)&&(tab_j[id_joueur].p[1].d[3]==1))||(!strcmp(rep,"r")&&(!pile_est_vide(pile_p)))||(!strcmp(rep,"q"))));
-
-	for(i=0;i<NB_PIONS;i++){
-		commande_pion[1]=48+i+1;
-		if((!strcmp(rep,commande_pion))&&(tab_j[id_joueur].p[i].d[0]==1))	sortir(tab_j,nb_joueurs,id_joueur,i);
-		if((!strcmp(rep,commande_pion))&&(tab_j[id_joueur].p[i].d[1]==1))	tour(tab_j,nb_joueurs,id_joueur,i,de);
-		if((!strcmp(rep,commande_pion))&&(tab_j[id_joueur].p[i].d[2]==1))	centre(tab_j,nb_joueurs,id_joueur,i,de);
-	}
-
-	if((!strcmp(rep,"r"))&&(!pile_est_vide(pile_p))) *retour=1;
-	else empiler_partie(pile_p,partie);
-
-	if(!strcmp(rep,"q")) *quitter_partie=1;
-	for(i=0;i<NB_PIONS;i++){
-		if(tab_j[id_joueur].p[0].centre==6) test_victoire++;
-	}
-	if(test_victoire==NB_PIONS) *victoire=1;	/*si les deux pions sont dans la case 6 du centre le joueur a gagné*/
-
-	/*on remet les drapeaux a 0*/
-	for(i=0;i<NB_PIONS;i++){
-		for(j=0;j<TAILLE_LISTE_DRAPEAUX;j++) tab_j[id_joueur].p[i].d[j]=0;
-	}
-}
 
 
 
